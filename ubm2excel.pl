cf3e33c88ae0722ab928dd19e15cb10ee1600c72
vlad.vul@gmail.com <Vlad Vul>
2014-09-25 14:51:02 +0400

    lol

--- ubm2excel.pl
+++ ubm2excel.pl
@@ -5,7 +5,7 @@
 # comment 3
 
 my $filename = $ARGV[0] || 'output.xlsx';
-use encoding "ru_RU.CP1251";
+#use encoding "ru_RU.CP1251";
 #use Encode;
 
 use Excel::Writer::XLSX;
@@ -20,17 +20,11 @@ my $header_format = $workbook->add_format(); $header_format->set_bold(); $header
 my @ip_header   = qw/ИсходящийIP Имя ВходящийIP Имя Мбайт/;
 my @time_header = qw/Время Мбайт/;
 my @port_header = qw/Порт  Мбайт/;
-# Add a worksheet
-<<<<<<< HEAD
-my $ip_vkl   = $workbook->add_worksheet('IP адреса');    $ip_vkl->write_row  ('A1', [ 'ИсходящийIP', 'Имя', 'ВходящийIP', 'Имя', 'Мбайт' ], $header_format);
-my $time_vkl = $workbook->add_worksheet('Время');        $time_vkl->write_row('A1', [ 'Время', 'Мбайт' ], $header_format ); 
-my $port_vkl = $workbook->add_worksheet('Порты');        $port_vkl->write_row('A1', [ 'Время', 'Мбайт' ], $header_format); 
-=======
+# Add a worksheet headers format
 my $ip_vkl   = $workbook->add_worksheet('IP адреса');    $ip_vkl->write_row  ('A1', \@ip_header,   $header_format);
 my $time_vkl = $workbook->add_worksheet('Время');        $time_vkl->write_row('A1', \@time_header, $header_format ); 
 my $port_vkl = $workbook->add_worksheet('Порты');        $port_vkl->write_row('A1', \@port_header, $header_format); 
 $ip_vkl->set_column(0, 3, 40); # first 4 column width set to 40
->>>>>>> 5d66730b1ff8c1663d57a00cac5722d1d8c00141
 
 my (%TIME, %IP, %PORT);
 
@@ -38,6 +32,8 @@ my (%TIME, %IP, %PORT);
 while (<>)
 {    chomp;
 	 my ($bytes, $src, $dst, $port1, $port2, $time) = m/h:(\d+).*A:(\S+).*B:(\S+).*a:(\S+).*b:(\S+).*E:(..........)/;
+	 next unless length "$bytes";
+	 next unless length "$src"; 	 next unless length "$dst"; 
 	 $time =~ s/(....)(..)(..)(..)/$1-$2-$3 $4:00/;
 	 $IP{$src."_".$dst} += $bytes;
 	 $PORT{$port1} += $bytes;
@@ -55,6 +51,7 @@ sub hashref2arrayref
 		$hash->{$key} /= 1024*1024;
 		if (my ($src_ip, $dst_ip) = split /_/, $key) # this is ip
 		{
+			warn 'src_ip '. $src_ip;
 			push @$arrayref, [ $src_ip, resolve($src_ip), $dst_ip, resolve($dst_ip), $hash->{$key} ];
 		};
 		push @$arrayref, [ $key, $hash->{$key} ];
@@ -68,42 +65,21 @@ $ip_vkl->write  ('A2', hashref2arrayref(\%IP));
 $time_vkl->write('A2', hashref2arrayref(\%TIME));
 $port_vkl->write('A2', hashref2arrayref(\%PORT));
 
-#    $format->set_color( 'red' );
-#    $format->set_align( 'center' );
-
-# Write a formatted and unformatted string, row and column notation.
-#$col = $row = 0;
-#$worksheet->write( $row, $col, 'Hi Excel!', $format );
-#$worksheet->write( 1, $col, 'Hi Excel!' );
-
-# Write a number and a formula using A1 notation
-#$worksheet->write( 'A3', 1.2345 );
-#$worksheet->write( 'A4', '=SIN(PI()/4)' );
-
-#sub array2excel
-#{
-#	
-#}
-
 sub hash_comp {   $TRAF->{$b} <=> $TRAF->{$a}   }
-
-#sub cp1251 { encode('cp1251', shift) }
 
 my %RESOLVE;
 
-<<<<<<< HEAD
-sub cp1251 { encode('1251', shift) }
-=======
+#sub cp1251 { encode('1251', shift) }
 sub resolve
 {
 	my $ip = shift;
 	if ((1+length $ip) < 5) {return ''}
-	if ( length $RESOLVE{$ip} ) { return $RESOLVE{$ip} } # cached
+	if ( defined $RESOLVE{$ip} ) 
+		{ return $RESOLVE{$ip} } # cached
 	$RESOLVE{$ip} = `host $ip|head -1| cut -f 5 -d ' ' | sed 's/\.\$//'`;
 	if (! length $RESOLVE{$ip} or $RESOLVE{$ip} eq '3(NXDOMAIN)')
-		{ return 'неизвестно' } # не шмогла
+		{ $RESOLVE{$ip} = 'неизвестно' } # не шмогла
 	return $RESOLVE{$ip};
 }
->>>>>>> 5d66730b1ff8c1663d57a00cac5722d1d8c00141
 
 
