a45f9d4be9df0cee751c8caa1a96af95635d10c7
Vlad.Vul <xoid@ya.ru>
2014-10-01 12:48:37 -0400

    relolvegood

--- ubm2excel.pl
+++ ubm2excel.pl
@@ -1,85 +1,96 @@
 #!/usr/bin/perl -w
 
-# comment 1
-# comment 2
-# comment 3
-
+use strict;
 my $filename = $ARGV[0] || 'output.xlsx';
-#use encoding "ru_RU.CP1251";
-#use Encode;
+my $top_rows = 20;
 
+use Socket;
 use Excel::Writer::XLSX;
 use utf8;
 
+my %RESOLVE;
 # Create a new Excel workbook
 my $workbook = Excel::Writer::XLSX->new($filename) or die $!;
 #  Add and define a format
+my $format = $workbook->add_format(); $format->set_num_format('0.000');
 my $header_format = $workbook->add_format(); $header_format->set_bold(); $header_format->set_align( 'center' );
 
-# headers
-my @ip_header   = qw/ИсходящийIP Имя ВходящийIP Имя Мбайт/;
-my @time_header = qw/Время Мбайт/;
-my @port_header = qw/Порт  Мбайт/;
-# Add a worksheet headers format
-my $ip_vkl   = $workbook->add_worksheet('IP адреса');    $ip_vkl->write_row  ('A1', \@ip_header,   $header_format);
-my $time_vkl = $workbook->add_worksheet('Время');        $time_vkl->write_row('A1', \@time_header, $header_format ); 
-my $port_vkl = $workbook->add_worksheet('Порты');        $port_vkl->write_row('A1', \@port_header, $header_format); 
-$ip_vkl->set_column(0, 3, 40); # first 4 column width set to 40
+my $all_vkl = $workbook->add_worksheet('Весь траффик');    
+my @all_header = qw/Время ИсходящийIP Имя ВходящийIP Имя ИсходящийПорт ВходящийПорт Байт/;
+$all_vkl->write_row  ('A1', \@all_header,   $header_format);
+$all_vkl->set_column(0, 13, 15); # column width
 
-my (%TIME, %IP, %PORT);
+my @ip_header = qw/ИсходящийIP Имя ВходящийIP Имя Mb/; 
+my $ip_vkl = $workbook->add_worksheet('TOP IP');    
+$ip_vkl->write_row  ('A1', \@ip_header,   $header_format); 
+$ip_vkl->set_column(0, 13, 15); # column width 
+my %IP;
+
+my @day_header = qw/Дата Mb/; 
+my $day_vkl = $workbook->add_worksheet('TOP date');    
+$day_vkl->write_row  ('A1', \@day_header,   $header_format); 
+$day_vkl->set_column(0, 13, 15); # column width 
+my %DAY;
 
 # read ubm from stdin
-while (<>)
-{    chomp;
+my $i = 2; # insert content from this row
+while (<STDIN>)
+{    chomp; 	# print '.';
 	 my ($bytes, $src, $dst, $port1, $port2, $time) = m/h:(\d+).*A:(\S+).*B:(\S+).*a:(\S+).*b:(\S+).*E:(..........)/;
 	 next unless length "$bytes";
-	 next unless length "$src"; 	 next unless length "$dst"; 
+	 next unless length "$src"; 	 
+	 next unless length "$dst"; 
 	 $time =~ s/(....)(..)(..)(..)/$1-$2-$3 $4:00/;
-	 $IP{$src."_".$dst} += $bytes;
-	 $PORT{$port1} += $bytes;
-	 $TIME{$time} += $bytes;
+	 my $name1 = resolve($src);
+	 my $name2 = resolve($dst);
+	 my @row = ($time, $src, $name1, $dst, $name2, $port1, $port2, $bytes);
+	 $all_vkl->write ('A'.$i++, \@row) ;
+	 $IP{$src.'_'.$dst} += $bytes;
+	 #warn "";
+	 $DAY{"$1-$2-$3"} += $bytes;
 }
 
-
-sub hashref2arrayref
+# group by ip src_dst 
+my $y = 2;
+foreach my $src_dst (sort_by_val(\%IP))
 {
-	my $hash = shift;
-	my $arrayref = [];
-	foreach my $key (keys %$hash)
-	{
-		#warn '.';
-		$hash->{$key} /= 1024*1024;
-		if (my ($src_ip, $dst_ip) = split /_/, $key) # this is ip
-		{
-			warn 'src_ip '. $src_ip;
-			push @$arrayref, [ $src_ip, resolve($src_ip), $dst_ip, resolve($dst_ip), $hash->{$key} ];
-		};
-		push @$arrayref, [ $key, $hash->{$key} ];
-	}
-	# sort by last value
-	return sort { $a->[-1] <=> $b->[-1] } @$arrayref;
+	my ($src, $dst) = split /_/, $src_dst;
+	#warn $src_dst, $IP{$src_dst};
+	my @row = ($src, resolve($src), $dst, resolve($dst), $IP{$src_dst}/1024/1024);
+	$ip_vkl->write('A'.$y++, \@row, $format);
+	last if ($y > $top_rows + 2);
 }
 
-# write to worksheets
-$ip_vkl->write  ('A2', hashref2arrayref(\%IP));
-$time_vkl->write('A2', hashref2arrayref(\%TIME));
-$port_vkl->write('A2', hashref2arrayref(\%PORT));
+$y = 2;
+foreach my $day (sort_by_val(\%DAY))
+{
+	my @row = ($day, $DAY{$day}/1024/1024);
+	$day_vkl->write('A'.$y++, \@row);
+	last if ($y > $top_rows + 2);
+}
 
-sub hash_comp {   $TRAF->{$b} <=> $TRAF->{$a}   }
 
-my %RESOLVE;
-
-#sub cp1251 { encode('1251', shift) }
+$workbook->close() or die "Error closing file: $!";
 sub resolve
-{
+{	#return 'lol';
 	my $ip = shift;
 	if ((1+length $ip) < 5) {return ''}
 	if ( defined $RESOLVE{$ip} ) 
 		{ return $RESOLVE{$ip} } # cached
-	$RESOLVE{$ip} = `host $ip|head -1| cut -f 5 -d ' ' | sed 's/\.\$//'`;
-	if (! length $RESOLVE{$ip} or $RESOLVE{$ip} eq '3(NXDOMAIN)')
-		{ $RESOLVE{$ip} = 'неизвестно' } # не шмогла
+	$RESOLVE{$ip} = gethostbyaddr( inet_aton($ip), AF_INET) || ' ';
+	#warn $RESOLVE{$ip};
+	#warn substr($RESOLVE{$ip}, 0, 3);
+	if ( substr($RESOLVE{$ip}, 0, 3) eq '3(N' )
+		{ $RESOLVE{$ip} = '' } # не шмогла
 	return $RESOLVE{$ip};
 }
 
 
+
+sub sort_by_val
+{
+	my $hash = shift;
+	use Data::Dumper;
+	#warn Dumper 'hash ', $hash, 'keys ';
+	return  sort { $hash->{$b} <=> $hash->{$a} } keys %$hash;
+}
